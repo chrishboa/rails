7b4b7145bad5edc4d24370c9d2e84134bdd76118
Carlos <csanta.v@gmail.com>
2013-04-11 13:36:42 -0300

    Erster commit, test src

---
+++ src/league_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeagueTest::Application.config.secret_token = 'b18e1f041696e701e691be9104fb912880d4441a26abbe1b7cb0e1e3fd2150ea5d76a3bcaf65d938b8110978331a0cac143d84341834d0c85a70e5968a9fe4d3'
